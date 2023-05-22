c8780ff540372cbab02170b2c833524f898af353
unknown <46878457@comunidadort.edu.ar>
2023-05-22 20:50:25 -0300

    borrado node modules

--- index-individuales/index-delete.js
+++
@@ -1,6 +0,0 @@
-
-import PizzaService from '../src/services/pizzas-services.js';
-
-let svc = new PizzaService();
-let resultDelete = await svc.deleteById(1);
-console.log(resultDelete)
--- index-individuales/index-update.js
+++
@@ -1,6 +0,0 @@
-import PizzaService from '../src/services/pizzas-services.js';
-
-let svc = new PizzaService();
-
- let resultUpdate = await svc.Update(2,"Tiago",1,200,"buena")
- console.log(resultUpdate)
